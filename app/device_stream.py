47c175fa5d8e6e23e1127ac95c7b49efb28968ec
Bones <cory@callref.com.au>
2023-07-11 16:57:27 +1000

    wrap redis stream in try catch block, adjust compose as env not passing some values correctly.

--- app/device_stream.py
+++ app/device_stream.py
@@ -45,44 +45,55 @@ async def stream_requests():
     stream_key = "api:stream:request"
     last_id = '0'
     while True:
-        resp = await rdb.xread({stream_key: last_id}, count=1, block=0)
-        for message in resp[0][1]:
-            last_id = message[0]
+        try:
+            resp = await rdb.xread({stream_key: last_id}, count=1, block=0)
+            for message in resp[0][1]:
+                last_id = message[0]
 
-            if b'request' in message[1]:
-                msg = message[1][b'request']
-                # print(b)
-                pl = api.request_log_pb2.RequestLog()
-                pl.ParseFromString(msg)
-                # print(MessageToujson(pl))
-                req = ujson.loads(MessageToJson(pl))
-                if 'method' not in req.keys():
-                    continue
+                if b'request' in message[1]:
+                    msg = message[1][b'request']
+                    # print(b)
+                    pl = api.request_log_pb2.RequestLog()
+                    pl.ParseFromString(msg)
+                    # print(MessageToujson(pl))
+                    req = ujson.loads(MessageToJson(pl))
+                    if 'method' not in req.keys():
+                        continue
+                    #if 'device_profile_id' in req.keys():
+                    #    continue
 
-                match req['method']:
-                    case 'Create':
-                        print('====== [Create Device Euis...] ======')
-                        print(MessageToJson(pl))
-                        await add_device_euis(req['metadata'])
-                    case 'Delete':
-                        print('====== [Delete Device Euis...] ======')
-                        print(MessageToJson(pl))
-                        await remove_device_euis(req['metadata'])
-                    case 'Update':
-                        print('====== [Update Device Euis...] ======')
-                        print(MessageToJson(pl))
-                        await update_device_euis(req['metadata'])
+                    match req['method']:
+                        case 'Create':
+                            print('====== [Create Device Euis...] ======')
+                            print(MessageToJson(pl))
+                            await add_device_euis(req['metadata'])
+                        case 'Delete':
+                            print('====== [Delete Device Euis...] ======')
+                            print(MessageToJson(pl))
+                            await remove_device_euis(req['metadata'])
+                        case 'Update':
+                            print('====== [Update Device Euis...] ======')
+                            print(MessageToJson(pl))
+                            await update_device_euis(req['metadata'])
+
+
+        except Exception as exc:
+            print(f'Error: {exc}')
+            pass
 
 
 async def add_device_euis(data: dict):
     """
-    adds a device to hpr when added as a device within the chirpstack api or webui.
+    add device:
+      adds a device to hpr when added as a device within the chirpstack api or webui.
     """
+    if 'dev_eui' not in data.keys():
+        return
     device = data['dev_eui']
     dev_eui, join_eui = await route_handler(device)
     print(dev_eui, join_eui)
     # cmd = f'hpr route euis list --route-id {route_id}'
-    cmd = f'hpr route euis add -d {dev_eui} -a {join_eui} --route-id {route_id}'
+    cmd = f'hpr route euis add -d {dev_eui} -a {join_eui} --route-id {route_id} --commit'
     res = helium_packet_router(cmd)
     return print(res)
 
@@ -93,6 +104,8 @@ async def remove_device_euis(data: dict):
       save device and join euis to a lookup db, as they're deleted and are unable to be called
       on delete from chirpstack devices. for now device euis will need to be manually removed.
     """
+    if 'dev_eui' not in data.keys():
+        return
     device = data['dev_eui']
     print(device)
     print(ujson.dumps(data, indent=2))
@@ -106,16 +119,18 @@ async def update_device_euis(data: dict):
       - enabling a device will add it back to the route id.
       - toggling on/off should allow for a previously unconnected device to be added to hpr.
     """
+    if 'dev_eui' not in data.keys():
+        return
     device = data['dev_eui']
     is_disabled = data['is_disabled']
     dev_eui, join_eui = await route_handler(device)
     # print('updating... ', dev_eui, join_eui)
     if is_disabled == 'true':
         # print('disabled, remove from route.. logic..')
-        cmd = f'hpr route euis remove -d {dev_eui} -a {join_eui} --route-id {route_id}'
+        cmd = f'hpr route euis remove -d {dev_eui} -a {join_eui} --route-id {route_id} --commit'
     else:
         # print('enabled, add back to route.. logic..')
-        cmd = f'hpr route euis add -d {dev_eui} -a {join_eui} --route-id {route_id}'
+        cmd = f'hpr route euis add -d {dev_eui} -a {join_eui} --route-id {route_id} --commit'
     res = helium_packet_router(cmd)
     return print(res)
 
