3205deb922169f5f0480fd1010fbdddc5925f673
Robert Putt <Robert.Putt@C02FV3BTMD6R.local>
2023-11-04 16:47:42 +0000

    Add in optional different postgres port and ssl mode

--- app/ChirpHeliumTenant.py
+++ app/ChirpHeliumTenant.py
@@ -22,6 +22,8 @@ class ChirpstackTenant:
             postgres_user: str,
             postgres_pass: str,
             postgres_name: str,
+            postgres_port: str,
+            postgres_ssl_mode: str,
             chirpstack_host: str,
             chirpstack_token: str,
     ):
@@ -30,7 +32,9 @@ class ChirpstackTenant:
         self.pg_user = postgres_user
         self.pg_pass = postgres_pass
         self.pg_name = postgres_name
-        self.postges = f'postgresql://{self.pg_user}:{self.pg_pass}@{self.pg_host}/{self.pg_name}'
+        self.pg_port = postgres_port
+        self.pg_ssl_mode = postgres_ssl_mode
+        self.postges = f'postgresql://{self.pg_user}:{self.pg_pass}@{self.pg_host}:{self.pg_port}/{self.pg_name}?sslmode={self.pg_ssl_mode}'
         self.cs_gprc = chirpstack_host
         self.auth_token = [('authorization', f'Bearer {chirpstack_token}')]
 
