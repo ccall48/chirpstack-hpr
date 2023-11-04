9566ead2afdf0f1950a7fa2e7ac1cb2be1efda91
Robert Putt <Robert.Putt@C02FV3BTMD6R.local>
2023-11-04 21:56:26 +0000

    Publish to SQS queue

--- app/SqsUsagePublisher.py
+++ app/SqsUsagePublisher.py
@@ -1,5 +1,9 @@
 import boto3
+import os
+import json
 import logging
+
+from botocore.exceptions import ClientError
 
 
 def info_log(msg):
@@ -7,4 +11,25 @@ def info_log(msg):
 
 
 def publish_to_sqs(event):
-    info_log("Publishing usage event to SQS: %s" % event)
+    queue_url = os.getenv('PUBLISH_USAGE_EVENTS_SQS_URL')
+    aws_region = os.getenv("PUBLISH_USAGE_EVENTS_SQS_REGION", "us-east-1")
+    info_log("Publishing usage event to SQS (%s): %s" % (queue_url, event))
+
+    sqs_client = boto3.client("sqs", region_name=aws_region)
+    dedup_id = '%s-%s-%s' % (
+        event.get('datetime'),
+        event.get('dev_eui'),
+        event.get('application_id')
+    )
+
+    try:
+        response = sqs_client.send_message(
+            QueueUrl=queue_url,
+            MessageBody=json.dumps(event),
+            MessageDeduplicationId=dedup_id,
+            MessageGroupId='HeliumUsageEvents'
+        )
+    except ClientError:
+        info_log(f'Could not send meessage to the - {queue_url}.')
+    else:
+        return response
