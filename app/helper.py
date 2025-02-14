2b7a6df4603aa633108ac5da9519a4a4308416ec
Bones <cory@callref.com.au>
2025-02-14 19:10:23 +1100

    add helper file for uplink byte counter

---
+++ app/helper.py
@@ -0,0 +1,12 @@
+from math import ceil
+
+
+def data_bytes_size(b64string: str) -> int:
+    """
+    get byte count from a given base64 string from its length if 0 return 1.
+    inital example: https://stackoverflow.com/a/6793638
+    """
+    dc = ceil(((len(b64string)*3)/4)/24)
+    if dc == 0:
+        return 1
+    return dc
