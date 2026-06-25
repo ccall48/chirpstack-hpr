2c8835f8431e028e702fd28cbdf6b02cf9ad853c
Bones <cory@callref.com.au>
2026-06-25 05:23:09 +0000

    refine base64 uplink bytes count

--- app/helium_func.py
+++ app/helium_func.py
@@ -8,10 +8,13 @@ def data_bytes_size(b64string: str) -> int:
     get byte count from a given base64 string from its length if 0 return 1.
     inital example: https://stackoverflow.com/a/6793638
     """
-    dc = ceil(((len(b64string)*3)/4)/24)
-    if dc == 0:
+    helium_bytes_per_dc = 24
+    # dc = ceil(((len(b64string)*3)/4)/24)
+    dc_used = ceil(((len(b64string) - b64string.count('='))*3//4)/helium_bytes_per_dc)
+    if dc_used == 0:
+        # we still get chatged per uplink if msg <= 0 bytes
         return 1
-    return dc
+    return dc_used
 
 
 def get_time():
